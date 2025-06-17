2f8427ef6c9a820e27c883ff7930d5b3d0f78f7c
Ashwini <suryavanshi264@gmail.com>
2025-06-17 16:54:48 +0530

    first commit

---
+++ Comments.py
@@ -0,0 +1,17 @@
+import pyjokes
+
+# print("jokes")
+joke=pyjokes.get_joke()
+print(joke)
+
+
+'''
+i am ashwini shingate from avasari
+how r u now??
+i am fine buddy
+i am going to goa next week are u ready to come with us?
+if you want to take permission from dad, still you have 15days of time pls ask him and let us know..
+'
+best regards,
+ashwini suryavanshi
+'''
