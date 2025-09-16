0684e973624e577bb6836c84945e11354f35927a
Ashwini <suryavanshi264@gmail.com>
2025-09-16 15:57:59 +0530

    Sep16-Day1

---
+++ Basics/Variable.py
@@ -0,0 +1,38 @@
+print("Variable types in Python")
+x=10
+print(x)
+print(type(x))
+x=3.14
+print(x)
+print(type(x))
+x=1+2j
+print(x)  # complex number
+print(type(x))  # complex type
+x=True  # boolean type      
+print(x)
+print(type(x))  # boolean type
+x="Hello, Python!"  # string type       
+print(x)
+print(type(x))  # string type
+x=['apple','banana','cherry']  # list type
+print(x)
+print(type(x))  # list type
+x=('apple','banana','cherry')  # tuple type
+print(x)
+print(type(x))  # tuple type
+x={'name':'Alice','age':25}  # dictionary type
+print(x)
+print(type(x))  # dictionary type
+x={'apple','banana','cherry'}  # set type
+print(x)
+
+print(type(x))  # set type
+x=frozenset({'apple','banana','cherry'})  # frozenset type
+print(x)
+print(type(x))  # frozenset type
+x=range(5)  # range type
+print(x)
+print(type(x))  # range type
+x=b'Hello'  # bytes type
+print(x)
+print(type(x))  # bytes type
